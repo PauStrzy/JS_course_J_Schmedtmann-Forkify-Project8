96feffc3a34c1c3eac80098e75593bd9df32c352
Paulina <paustrzy@gmail.com>
2022-12-30 22:25:25 +0100

    Created forkify app

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9da3589f-7a95-4b3f-871c-1b8247e6ec19';
+export const MODAL_CLOSE_SEC = 2.5;
